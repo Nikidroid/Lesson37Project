7f8585c34bb162da511c5dbe6813a9ae06fc4200
Krotovich Krot <for.games.straus3@gmail.com>
2024-02-01 18:19:47 +0300

    Add sum function.

--- Lesson37Project/Main.cpp
+++ Lesson37Project/Main.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
-int main() {
+int sum(int num1, int num2);
 
+int main() {
+	int num1, num2;
+	cout << "Enter two numbers: ";
+	cin >> num1 >> num2;
+
+	cout << "Sum: " << sum(num1, num2);
 	return 0;
 }
